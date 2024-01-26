ea415834ef1302949de6891ff44f884615e31ea6
Kitenok <75540213+KitenokGene@users.noreply.github.com>
2024-01-26 16:31:10 +0300

    Update DecryptStringsAction.cs

--- Supercell.ArxanUnprotector/Actions/DecryptStringsAction.cs
+++ Supercell.ArxanUnprotector/Actions/DecryptStringsAction.cs
@@ -1,4 +1,4 @@
-﻿namespace Supercell.ArxanUnprotector.Actions;
+namespace Supercell.ArxanUnprotector.Actions;
 
 using Supercell.ArxanUnprotector;
 using Supercell.ArxanUnprotector.Ranges;
@@ -6,28 +6,36 @@ using Supercell.ArxanUnprotector.Strings;
 
 public class DecryptStringsAction : IAction
 {
-    public string Execute(Library original, Library modified, string output)
-    {
-        if (original == null && modified == null)
-            return "No input library provided.";
-        if (original != null && modified != null)
-            return "Cannot decrypt strings in both original and modified libraries";
-        
-        Library input = original ?? modified;
+	public string Execute(Library original, Library modified, string output)
+	{
+		if (original == null && modified == null)
+			return "No input library provided.";
+		if (original != null && modified != null)
+			return "Cannot decrypt strings in both original and modified libraries";
 
-        RangeTable encryptedStringRangeTable = input.EncryptedStringRangeTable;
-        EncryptedStringKey encryptedStringKey = input.EncryptedStringKey;
-        
-        if (encryptedStringRangeTable == null)
-            return "No encrypted string ranges found";
-        if (encryptedStringKey.Address == 0)
-            return "No encrypted string key found";
-        if (!input.IsStringsEncrypted)
-            return "Strings already decrypted";
+		Library input = original ?? modified;
 
-        input.DecryptStrings();
-        input.Save(output);
+		RangeTable encryptedStringRangeTable = input.EncryptedStringRangeTable;
+		EncryptedStringKey encryptedStringKey = input.EncryptedStringKey;
 
-        return null;
-    }
+		if (encryptedStringRangeTable == null)
+			return "No encrypted string ranges found";
+
+		if (encryptedStringKey.Address == 0)
+		{
+			return "No encrypted string key found";
+		}
+		else
+		{
+			Console.WriteLine("XOR key: " + BitConverter.ToString(encryptedStringKey.Content.ToArray()).Replace("-", string.Empty));
+		}
+
+		if (!input.IsStringsEncrypted)
+			return "Strings already decrypted";
+
+		input.DecryptStrings();
+		input.Save(output);
+
+		return null;
+	}
 }
