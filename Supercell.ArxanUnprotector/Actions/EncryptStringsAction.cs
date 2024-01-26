2b46a8ed7008254df870136805f730012f9653a5
Kitenok <75540213+KitenokGene@users.noreply.github.com>
2024-01-26 16:33:42 +0300

    Update EncryptStringsAction.cs

--- Supercell.ArxanUnprotector/Actions/EncryptStringsAction.cs
+++ Supercell.ArxanUnprotector/Actions/EncryptStringsAction.cs
@@ -1,4 +1,4 @@
-﻿namespace Supercell.ArxanUnprotector.Actions;
+namespace Supercell.ArxanUnprotector.Actions;
 
 using Supercell.ArxanUnprotector;
 using Supercell.ArxanUnprotector.Ranges;
@@ -20,9 +20,17 @@ public class EncryptStringsAction : IAction
         
         if (encryptedStringRangeTable == null)
             return "No encrypted string ranges found";
-        if (encryptedStringKey.Address == 0)
-            return "No encrypted string key found";
-        if (input.IsStringsEncrypted)
+
+		if (encryptedStringKey.Address == 0)
+		{
+			return "No encrypted string key found";
+		}
+		else
+		{
+			Console.WriteLine("XOR key: " + BitConverter.ToString(encryptedStringKey.Content.ToArray()).Replace("-", string.Empty));
+		}
+
+		if (input.IsStringsEncrypted)
             return "Strings already encrypted";
 
         input.EncryptedStringKey.Randomize();
